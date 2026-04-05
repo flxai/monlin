c234ec606b4c46bee41bd6ee7c8e777f7c07bce5
Felix <git@flx.ai>
2026-04-05 05:17:58 +0200

    chore: bump version to 0.6.2

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
       pkgs = nixpkgs.legacyPackages.${system};
       staticPkgs = pkgs.pkgsStatic;
       commonArgs = {
-        version = "0.6.1";
+        version = "0.6.2";
         src = self;
         cargoLock.lockFile = ./Cargo.lock;
         nativeBuildInputs = [pkgs.installShellFiles];
