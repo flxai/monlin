979dfb865de82d52eb799ca251f9adeec2e6bc6f
Felix <git@flx.ai>
2026-04-19 22:49:30 +0200

    chore(release): bump version to 0.6.5

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
       pkgs = nixpkgs.legacyPackages.${system};
       staticPkgs = pkgs.pkgsStatic;
       commonArgs = {
-        version = "0.6.4";
+        version = "0.6.5";
         src = self;
         cargoLock.lockFile = ./Cargo.lock;
         nativeBuildInputs = [pkgs.installShellFiles];
